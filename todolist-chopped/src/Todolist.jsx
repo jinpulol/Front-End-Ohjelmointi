69701fbd829dd2c91d035da06694bfcad23ab6fe
jinpulol <jesselehmonen@gmail.com>
2023-10-03 23:48:18 +0300

    separating components

---
+++ todolist-chopped/src/Todolist.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import TodoTable from "./TodoTable";
+
+export default function Todolist() {
+
+    const [todos, setTodos] = useState([])
+    const [todo, setTodo] = useState({ descr: "", date: "" })
+
+    const handleInputChanged = (event) => {
+        event.preventDefault();
+        setTodo({ ...todo, [event.target.name]: event.target.value })
+    }
+
+    const addTodo = (event) => {
+        event.preventDefault();
+        setTodos([...todos, todo])
+    }
+
+    const deleteTodo = (event) => {
+        event.preventDefault();
+        setTodos(todos.filter((todo, index) => parseInt(event.target.id) !== index))
+    }
+
+    return (
+        <>
+            <h1>Todo list</h1>
+            <input
+                type="text"
+                name="descr"
+                value={todo.descr}
+                onChange={handleInputChanged}
+            />
+            <input
+                type="date"
+                name="date"
+                value={todo.date}
+                onChange={handleInputChanged}
+            />
+            <button
+                onClick={addTodo}>
+                Add
+            </button>
+            <TodoTable todos={todos} deleteTodo={deleteTodo} />
+        </>
+    )
+
+}
